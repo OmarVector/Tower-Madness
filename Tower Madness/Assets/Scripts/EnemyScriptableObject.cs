3a21b891ada91a4ee13121aa92a4fa76dfbb8a9b
Omar Vector <omar.ahmed.vc@gmail.com>
2019-11-20 18:27:48 +0100

    Add : Enemy Scriptable Object
    Add : Basic,Elite and Boss enemies prefabs
    Refactor : Prefab Folder

---
+++ Tower Madness/Assets/Scripts/EnemyScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+[CreateAssetMenu(menuName = "Gameplay Scriptable Objects/Enemy ")]
+public class EnemyScriptableObject : ScriptableObject
+{
+   public int Health;
+   public float Speed;
+   public float Damage;
+   public int GoldScore;
+}
