54b7f1cac992d51f4dfe057eb0f5357b634144ca
Omar Vector <omar.ahmed.vc@gmail.com>
2019-11-21 08:01:22 +0100

    Add : Player Castle Template
    Update: Physics layers

---
+++ Tower Madness/Assets/Scripts/PlayerCastle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+public class PlayerCastle : MonoBehaviour
+{
+    [SerializeField] private PlayerCastleScriptableObject playerSettings;
+  
+    [HideInInspector] public int Health;
+    [HideInInspector] public int StartedCoins;
+
+    void InitializeSettings()
+    {
+        Health = playerSettings.Health;
+        StartedCoins = playerSettings.StartingCoins;
+    }
+    
+    private void Awake()
+    {
+       InitializeSettings();
+    }
+
+    private void OnTriggerEnter(Collider enemy)
+    {
+        //TODO
+        Health -= 100;
+        Debug.Log(Health);
+    }
+
+ 
+}
---
+++ Tower Madness/Assets/Scripts/PlayerCastleScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gameplay Scriptable Objects/Player Castle Settings ")]
+public class PlayerCastleScriptableObject : ScriptableObject
+{
+   public int Health;
+   public int StartingCoins;
+}
