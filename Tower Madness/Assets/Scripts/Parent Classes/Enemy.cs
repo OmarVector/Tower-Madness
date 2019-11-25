de574a5a74494c43bb01bf98008b2621aedf64cc
Omar Vector <omar.ahmed.vc@gmail.com>
2019-11-25 07:20:21 +0100

    Add: ToolTips to Enemy script and commenting it out

--- Tower Madness/Assets/Scripts/Parent Classes/Enemy.cs
+++ Tower Madness/Assets/Scripts/Parent Classes/Enemy.cs
@@ -2,12 +2,19 @@
 using UnityEngine.AI;
 using UnityEngine.UI;
 
-
+/// <summary>
+/// This enemy class,could be extended to be a parent class ,
+/// Here, For each enemy type gameobject, should has its own Scriptable object to initialize its properties.
+/// </summary>
 public class Enemy : MonoBehaviour
 {
+    [Tooltip("Scriptable Object for Enemy properties")]
     [SerializeField] private EnemyScriptableObject enemySettings;
+    
+    [Tooltip("Health bar sprite")]
     [SerializeField] private Image healthBar;
 
+    // Struct that hold enemy properties
     public struct EnemyProperties
     {
         public int Health;
@@ -18,7 +25,7 @@ public class Enemy : MonoBehaviour
 
     [HideInInspector] public EnemyProperties enemyProperties;
 
-    // In case we need to override it later on.
+    // Initializing enemy properties , called on Awake 
     public void InitializeSettings()
     {
         enemyProperties.Health = enemySettings.Health;
@@ -31,6 +38,7 @@ public class Enemy : MonoBehaviour
 
 
     //TODO Memory Pooling
+    // its called when enemy hit by the tower. the function return true if enemy die, we need this boolean so we can kick the enemy out from tower range later.
     public bool HitByTower(int damage)
     {
         enemyProperties.Health -= damage;
@@ -41,10 +49,9 @@ public class Enemy : MonoBehaviour
             var amount = Random.Range(enemyProperties.GoldLowRange, enemyProperties.GoldHighRange);
             GameManager.gameManager.SetGold(amount);
                 
-            Destroy(gameObject);
+            Destroy(gameObject); //TODO , should return to bool 
             return true;
         }
-
 
         return false;
     }
