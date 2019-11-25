2d7a932e30f717b7c87bb5359063f9855eaf1800
Omar Vector <omar.ahmed.vc@gmail.com>
2019-11-25 07:12:19 +0100

    Add : ToolTips to TowerSO and UISO Scripts
    SO == ScriptableObject

---
+++ Tower Madness/Assets/Scripts/ScriptableObjects/TowerScriptableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+// TO create new one, just right click on any folder and check "GamePlay ScriptableObjects->Tower"
+[CreateAssetMenu(menuName = "Gameplay Scriptable Objects/Tower ")]
+public class TowerScriptableObject : ScriptableObject
+{
+   [Tooltip("Tower's Cost")]
+   public int Price;
+   
+   // it sets the collision sphere radius.
+   [Tooltip("Tower Fire Range")]
+   public float Range;
+   
+   [Tooltip("Attack rates per sec")]
+   public float ShootIntervals;
+   
+   [Tooltip("Tower's Damage to enemy")]
+   public int Damage;
+   
+   //TODO
+   public ParticleSystem weaponParticleSystem;
+   public ParticleSystem hittingTargetParticleSystem;
+}
---
+++ Tower Madness/Assets/Scripts/ScriptableObjects/UiScriptableObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+// TO create new one, just right click on any folder and check "GamePlay ScriptableObjects->UI Elements"
+[CreateAssetMenu(menuName = "Gameplay Scriptable Objects/UI Elements ")]
+public class UiScriptableObject : ScriptableObject
+{
+    [Tooltip("AAGun tower image sprite")]
+    public Sprite AAGunTowerIcon;
+    
+    [Tooltip("Laser tower image sprite")]
+    public Sprite LaserTowerIcon;
+    
+    [Tooltip("Flame tower image sprite")]
+    public Sprite FlameTowerIcon;
+    
+    [Tooltip("Rocket tower image sprite")]
+    public Sprite RocketTowerIcon;
+    
+    [Tooltip("Gold image sprite for global UI")]
+    public Sprite GoldIcon;
+    
+    [Tooltip("Health image sprite for global UI")]
+    public Sprite HealthIcon;
+}
