4b5a069e5d502d5e54585754380d287a54a2b061
Omar Vector <omar.ahmed.vc@gmail.com>
2019-11-25 07:00:09 +0100

    Add: Tool tips to enemy scriptable object

---
+++ Tower Madness/Assets/Scripts/ScriptableObjects/EnemyScriptableObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+// TO create new one, just right click on any folder and check "GamePlay ScriptableObjects->Enemy
+[CreateAssetMenu(menuName = "Gameplay Scriptable Objects/Enemy ")]
+public class EnemyScriptableObject : ScriptableObject
+{
+   [Tooltip("Health of the enemy")]
+   public int Health;
+   
+   [Tooltip("How fast is moving toward player castle")] 
+   public float Speed;
+   
+   [Tooltip("Damage it does to the player castle")]
+   public float Damage;
+   
+   [Tooltip("Min gold amount drop when it die")]
+   public int GoldLowRange;
+   
+   [Tooltip("Max gold amount drop when it die")]
+   public int GoldHighRange;
+}
